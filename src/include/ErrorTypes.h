4835df7fcc7534aad7e35930a1ab0aa9d7289d7e
Kuoppala Siiri <22868831+siiruli@users.noreply.github.com>
2023-03-12 18:26:43 +0200

    Move error types to a different file

---
+++ src/include/ErrorTypes.h
@@ -0,0 +1,120 @@
+#pragma once
+#include <variant>
+#include <vector>
+#include <iostream>
+#include <ostream>
+#include <sstream>
+#include "Token.h"
+#include "AstNode.h"
+
+
+template<class NodeType, class ErrorType>
+inline void addMeta(NodeType &node, ErrorType &error){
+  error.contextSpan = node.span;
+  error.context = astNodeName<NodeType>();
+}
+
+struct ErrorBase {
+  inline virtual std::string description(){
+    std::stringstream desc;
+    desc << errorClass() << ": ";
+    desc << shortDescription();
+    desc << " at " << span;
+    desc << " in " << context << " (" << contextSpan << ")";
+    return desc.str(); 
+  };
+  Span span;
+  Span contextSpan;
+  std::string context;
+
+  private:
+    virtual std::string errorClass() = 0;
+    virtual std::string shortDescription() = 0;
+};
+enum class ScanningErrorType {Eof, UnexpNewline, UnexpChar};
+struct ScanningError : ErrorBase {
+  ScanningErrorType type;
+  std::optional<char> ch;
+  Position pos;
+  inline virtual std::string errorClass() { return "Lexical error"; }
+  inline virtual std::string shortDescription(){
+    std::stringstream desc;
+    desc << "Unexpected ";
+    if(type == ScanningErrorType::UnexpChar){
+      desc << "character '" << ch.value_or(' ') << "'";
+    }else if(type == ScanningErrorType::Eof){
+      desc << "end-of-file"; 
+    }else if(type == ScanningErrorType::UnexpNewline){
+      desc << "newline"; 
+    }
+    return desc.str();
+  }
+  
+  
+};
+struct ParsingError : ErrorBase {
+  Token token;
+  std::string data;
+  inline virtual std::string errorClass() { return "Syntax error"; }
+  inline virtual std::string shortDescription(){ 
+    std::stringstream desc;
+    desc << "Unexpected ";
+    if(std::holds_alternative<VarIdent>(token.value)) 
+      desc << "identifier '" << token.value << "'";
+    else desc << token.value;
+    return desc.str();
+  }
+};
+enum class SemErrorType {Other, NotDeclared, ReDeclared, WrongScope, AssignConstant};
+struct SemanticError : ErrorBase {
+  VarIdent identifier;
+  SemErrorType type;
+  inline virtual std::string errorClass() { return "Semantic error"; }
+  inline virtual std::string shortDescription(){ 
+    switch (type)
+    {
+    case SemErrorType::NotDeclared:
+      return "Variable " + identifier + " used without declaration"; 
+    case SemErrorType::ReDeclared:
+      return "Redeclaration of variable " + identifier;
+    case SemErrorType::WrongScope:
+      return "Declaration not allowed inside if or for";
+    case SemErrorType::AssignConstant:
+      return "Variable " + identifier + " is constant inside for-loop";
+      break;
+    default:
+      return "Unkown";
+    }
+    return "";
+  }
+
+};
+struct TypeError : ErrorBase {
+
+  std::vector<Type> expected;
+  std::vector<Type> got;
+
+  inline virtual std::string errorClass() { return "Type error"; }
+  inline virtual std::string shortDescription(){
+    std::stringstream desc;
+    desc << "Unexpected type. Expected: ";
+    for(auto t : expected) desc << t << " ";
+    desc << ", got: ";
+    for(auto t : got) desc << t << " ";
+    return desc.str();
+  }
+};
+
+struct RuntimeError : ErrorBase {
+  std::string data;
+  inline virtual std::string errorClass() { return "Runtime error"; }
+  inline virtual std::string shortDescription(){ 
+    std::stringstream desc;
+    desc << data;
+    return desc.str();
+  }
+};
+
+typedef std::variant<
+  ScanningError, ParsingError, SemanticError, TypeError, RuntimeError
+> Error;
