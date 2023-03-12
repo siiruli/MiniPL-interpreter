ff9392b57e2c1463eb5154424bca8a743c4eb2ff
Kuoppala Siiri <22868831+siiruli@users.noreply.github.com>
2023-03-12 22:19:02 +0200

    Fix semantic analyzer from the big change

--- src/classes/SemanticAnalyzer.cpp
+++ src/classes/SemanticAnalyzer.cpp
@@ -1,153 +1,150 @@
-// #include <iostream>
-// #include <assert.h>
-// #include "SemanticAnalyzer.h"
+#include <iostream>
+#include <assert.h>
+#include "SemanticAnalyzer.h"
 
 
-// template<class NodeType> 
-// void SemanticAnalyzer::raiseError(NodeType &node){
-//   SemanticError error;
-//   error.type = SemErrorType::Other;
-//   error.contextSpan = node.span;
-//   raiseError(node, error);
-// }
+template<class NodeType> 
+void SemanticAnalyzer::raiseError(NodeType &node){
+  SemanticError error;
+  error.type = SemErrorType::Other;
+  error.contextSpan = node.span;
+  raiseError(node, error);
+}
 
-// template<class NodeType> 
-// void SemanticAnalyzer::raiseError(NodeType &node, VarIdent varId, SemErrorType type){
-//   SemanticError error;
-//   error.identifier = varId;
-//   error.contextSpan = node.span;
-//   error.type = type;
-//   raiseError(node, error);
-// }
+template<class NodeType> 
+void SemanticAnalyzer::raiseError(NodeType &node, VarIdent varId, SemErrorType type){
+  SemanticError error;
+  error.identifier = varId;
+  error.contextSpan = node.span;
+  error.type = type;
+  raiseError(node, error);
+}
 
-// template<class NodeType> 
-// void SemanticAnalyzer::raiseError(NodeType &node, SemanticError error){
-//   error.span = node.span;
-//   error.context = astNodeName<NodeType>();
-//   handler.raiseError(error);
-// }
-// void SemanticAnalyzer::visit(ExprAstNode &node){
+template<class NodeType> 
+void SemanticAnalyzer::raiseError(NodeType &node, SemanticError error){
+  error.span = node.span;
+  error.context = astNodeName<NodeType>();
+  handler.raiseError(error);
+}
 
-//   visit(*node.opnd1);
+
+
+void SemanticAnalyzer::visit(ExprNode &node){
+  std::visit(overloaded {
+    [&](LiteralNode &node) {
+    },
+    [&](VarNode &node) {
+      accessVar(node, node.varId);
+    },
+    [&](UnaryOp &node) {
+      visit(*node.opnd);
+    },
+    [&](BinaryOp &node) {
+      visit(*node.opnd1);
+      visit(*node.opnd2);
+    },
+  }, node);
+}
+void SemanticAnalyzer::visit(StatementsAstNode &node){
+  for(auto &child : node.statements){
+    visit(child);
+  }
+}
+
+void SemanticAnalyzer::visit(AstNode &node){
+  std::visit([&](auto &node){
+    visit(node);
+  }, node);
+}
+
+void SemanticAnalyzer::visit(IfAstNode &node){
+  visit(node.expr);
+  scopes.push_back(node.span);
+  visit(node.ifStatements);
+  visit(node.elseStatements);
+  scopes.pop_back();
+}
+void SemanticAnalyzer::visit(DeclAstNode &node){
+
+  if(auto &expr = node.value){
+    visit(*expr);
+  }
+  declareVar(node, node.varId);
+}
+void SemanticAnalyzer::visit(AssignAstNode &node){
+  assignVar(node, node.varId);
+  visit(node.expr);
+}
+
+void SemanticAnalyzer::visit(ForAstNode & node){
+  assignVar(node, node.varId);
+  visit(node.startExpr);
+  visit(node.endExpr);
   
-//   if(node.opnd2) {
-//     visit(*node.opnd2);
-//   }
+  scopes.push_back(node.span);
+  getVar(node.varId).forParent = &node;
+  visit(node.statements);
+  getVar(node.varId).forParent = nullptr;
+  scopes.pop_back();
+}
+void SemanticAnalyzer::visit(PrintAstNode & node){
+  visit(node.expr);
+}
+void SemanticAnalyzer::visit(ReadAstNode & node){
+  assignVar(node, node.varId);
+} 
 
-// }
+void SemanticAnalyzer::initVar(std::string varId){
+  variables[varId] = variable{};
+}
 
-// void SemanticAnalyzer::visit(OpndAstNode &node){
-//   std::visit( overloaded {
-//     [&](Literal &arg){
-//     },
-//     [&](VarIdent &arg){
-//       accessVar(node, arg);
-//     },
-//     [&](ExprAstNode &arg){
-//       visit(arg);
-//     }
-//   }, node.operand);
-// }
-// void SemanticAnalyzer::visit(StatementsAstNode &node){
-//   for(auto &child : node.statements){
-//     visit(child);
-//   }
-// }
+bool SemanticAnalyzer::hasVar(VarIdent varId){
+  return variables.count(varId);
+}
+variable & SemanticAnalyzer::getVar(VarIdent varId){
+  return variables[varId];
+}
 
-// void SemanticAnalyzer::visit(AstNode &node){
-//   std::visit([&](auto &node){
-//     visit(node);
-//   }, node);
-// }
+template<class NodeType> 
+void SemanticAnalyzer::declareVar(NodeType &node, VarIdent varId){
+  // initialize variable
+  if(hasVar(node.varId)) {
+    SemanticError error;
+    error.identifier = varId;
+    error.type = SemErrorType::ReDeclared;
+    raiseError(node, error); // redeclaration
+  }
+  if(!scopes.empty()){
+    SemanticError error;
+    error.identifier = varId;
+    error.type = SemErrorType::WrongScope;
+    error.contextSpan = scopes[0];
+    raiseError(node, error);
+  }
+  initVar(node.varId);
+}
+template<class NodeType> 
+void SemanticAnalyzer::assignVar(NodeType &node, VarIdent varId){
+  if(!hasVar(varId)) {
+    raiseError(node, node.varId, SemErrorType::NotDeclared);
+  }
+  else{
+    ForAstNode *forParent = getVar(varId).forParent;
+    if(forParent){
+      SemanticError error;
+      error.type = SemErrorType::AssignConstant;
+      error.contextSpan = forParent->span;
+      error.span = node.span;
+      error.context = astNodeName<ForAstNode>();
+      error.identifier = varId;
+      handler.raiseError(error);
+    }
+  }
 
-// void SemanticAnalyzer::visit(IfAstNode &node){
-//   visit(node.expr);
-//   scopes.push_back(node.span);
-//   visit(node.ifStatements);
-//   visit(node.elseStatements);
-//   scopes.pop_back();
-// }
-// void SemanticAnalyzer::visit(DeclAstNode &node){
-
-//   if(auto &expr = node.value){
-//     visit(*expr);
-//   }
-//   declareVar(node, node.varId);
-// }
-// void SemanticAnalyzer::visit(AssignAstNode &node){
-//   assignVar(node, node.varId);
-//   visit(node.expr);
-// }
-
-// void SemanticAnalyzer::visit(ForAstNode & node){
-//   assignVar(node, node.varId);
-//   visit(node.startExpr);
-//   visit(node.endExpr);
-  
-//   scopes.push_back(node.span);
-//   getVar(node.varId).forParent = &node;
-//   visit(node.statements);
-//   getVar(node.varId).forParent = nullptr;
-//   scopes.pop_back();
-// }
-// void SemanticAnalyzer::visit(PrintAstNode & node){
-//   visit(node.expr);
-// }
-// void SemanticAnalyzer::visit(ReadAstNode & node){
-//   assignVar(node, node.varId);
-// } 
-
-// void SemanticAnalyzer::initVar(std::string varId){
-//   variables[varId] = variable{};
-// }
-
-// bool SemanticAnalyzer::hasVar(VarIdent varId){
-//   return variables.count(varId);
-// }
-// variable & SemanticAnalyzer::getVar(VarIdent varId){
-//   return variables[varId];
-// }
-
-// template<class NodeType> 
-// void SemanticAnalyzer::declareVar(NodeType &node, VarIdent varId){
-//   // initialize variable
-//   if(hasVar(node.varId)) {
-//     SemanticError error;
-//     error.identifier = varId;
-//     error.type = SemErrorType::ReDeclared;
-//     raiseError(node, error); // redeclaration
-//   }
-//   if(!scopes.empty()){
-//     SemanticError error;
-//     error.identifier = varId;
-//     error.type = SemErrorType::WrongScope;
-//     error.contextSpan = scopes[0];
-//     raiseError(node, error);
-//   }
-//   initVar(node.varId);
-// }
-// template<class NodeType> 
-// void SemanticAnalyzer::assignVar(NodeType &node, VarIdent varId){
-//   if(!hasVar(varId)) {
-//     raiseError(node, node.varId, SemErrorType::NotDeclared);
-//   }
-//   else{
-//     ForAstNode *forParent = getVar(varId).forParent;
-//     if(forParent){
-//       SemanticError error;
-//       error.type = SemErrorType::AssignConstant;
-//       error.contextSpan = forParent->span;
-//       error.span = node.span;
-//       error.context = astNodeName<ForAstNode>();
-//       error.identifier = varId;
-//       handler.raiseError(error);
-//     }
-//   }
-
-// }
-// template<class NodeType> 
-// void SemanticAnalyzer::accessVar(NodeType &node, VarIdent varId){
-//   if(!hasVar(varId)) 
-//     raiseError(node, varId, SemErrorType::NotDeclared);
-// }
+}
+template<class NodeType> 
+void SemanticAnalyzer::accessVar(NodeType &node, VarIdent varId){
+  if(!hasVar(varId)) 
+    raiseError(node, varId, SemErrorType::NotDeclared);
+}
 
--- src/include/SemanticAnalyzer.h
+++ src/include/SemanticAnalyzer.h
@@ -1,60 +1,67 @@
-// #pragma once
-// #include <map>
-// #include "Util.h"
-// #include "AstNode.h"
-// #include "Error.h"
+#pragma once
+#include <map>
+#include "Util.h"
+#include "AstNode.h"
+#include "Error.h"
 
-// /** @file */ 
-// struct variable {
-//   variable() : declared{1} {}
-//   bool declared = 0;
-//   ForAstNode *forParent;
-// };
+/** @file */ 
+struct variable {
+  variable() : declared{1} {}
+  bool declared = 0;
+  ForAstNode *forParent;
+};
 
-// class SemanticAnalyzer {
-//   public:
-//     SemanticAnalyzer(ErrorHandler &handler) : handler(handler) {}
+class SemanticAnalyzer {
+  public:
+    SemanticAnalyzer(ErrorHandler &handler) : handler(handler) {}
 
-//     void visit(AstNode &node);
-//     void visit(IfAstNode &node);
-//     void visit(DeclAstNode &node);
-//     void visit(AssignAstNode &node);
-//     void visit(ForAstNode &node);
-//     void visit(PrintAstNode &node);
-//     void visit(ReadAstNode &node);
-//     void visit(StatementsAstNode &node);
-//     void visit(ExprAstNode &node);
-//     void visit(OpndAstNode &node);
-//   private:
-//     ErrorHandler &handler;
-    
-//     std::vector<Span> scopes;
+    void visit(AstNode &node);
+
+    void visit(ExprNode &node);
+    void visit(LiteralNode &node);
+    void visit(UnaryOp &node);
+    void visit(BinaryOp &node);
+    void visit(VarNode &node);
     
 
-//     template<class NodeType> 
-//     void raiseError(NodeType &node);
+    void visit(IfAstNode &node);
+    void visit(DeclAstNode &node);
+    void visit(AssignAstNode &node);
+    void visit(ForAstNode &node);
+    void visit(PrintAstNode &node);
+    void visit(ReadAstNode &node);
+    void visit(StatementsAstNode &node);
+
+  private:
+    ErrorHandler &handler;
     
-//     template<class NodeType> 
-//     void raiseError(NodeType &node, SemanticError error);
-
-//     template<class NodeType> 
-//     void raiseError(NodeType &node, VarIdent varId, SemErrorType type);
-
-
-//     std::map<VarIdent, variable> variables; 
-
-//     // use unique_ptr
-
-//     void initVar(VarIdent varId);
-//     bool hasVar(VarIdent varId);
-//     variable & getVar(VarIdent varId);
-
-//     template<class NodeType> 
-//     void declareVar(NodeType &node, VarIdent varId);
-//     template<class NodeType> 
-//     void assignVar(NodeType &node, VarIdent varId);
-//     template<class NodeType> 
-//     void accessVar(NodeType &node, VarIdent varId);
+    std::vector<Span> scopes;
     
 
-// };
+    template<class NodeType> 
+    void raiseError(NodeType &node);
+    
+    template<class NodeType> 
+    void raiseError(NodeType &node, SemanticError error);
+
+    template<class NodeType> 
+    void raiseError(NodeType &node, VarIdent varId, SemErrorType type);
+
+
+    std::map<VarIdent, variable> variables; 
+
+    // use unique_ptr
+
+    void initVar(VarIdent varId);
+    bool hasVar(VarIdent varId);
+    variable & getVar(VarIdent varId);
+
+    template<class NodeType> 
+    void declareVar(NodeType &node, VarIdent varId);
+    template<class NodeType> 
+    void assignVar(NodeType &node, VarIdent varId);
+    template<class NodeType> 
+    void accessVar(NodeType &node, VarIdent varId);
+    
+
+};
